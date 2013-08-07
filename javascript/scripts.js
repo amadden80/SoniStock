96ad487bed378b023d941f9c37050317539a8ec6
amadden80 <amadden80@gmail.com>
2013-08-07 08:57:11 -0400

    Print data to console

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,24 +1,32 @@
 
+
+
 function SoniStock_audio(symbol){
 
         var callback = function(data) {
             var opening = data.query.results.quote["Open"];
             var last_trade = data.query.results.quote["LastTradePriceOnly"];
-            Sonifizer_array([opening, last_trade], Sonifizer_Play)            
+
+            console.log("Symbol: " + symbol);
+            console.log("Opening: " + opening);
+            console.log("Last Trade: " + last_trade);
+            Sonifizer_array([opening, last_trade], Sonifizer_Play);
         };
-        var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+symbol+"%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
+
+        //  Stock prices optained from Yahoo Finance
+        var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+symbol+"%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json";
         $.getJSON(url, callback);
 }
 
 function submit_stock_audio(e){
     e.preventDefault();
-    var stock_qoute = $('#stock-input').val()
+    var stock_qoute = $('#stock-input').val();
     if (stock_qoute.length>0){
         SoniStock_audio(stock_qoute, Sonifizer_Play);
     }
 }
 
 $(function(){
-    $('#stock-play-button').on('click', submit_stock_audio)
+    $('#stock-play-button').on('click', submit_stock_audio);
 });
 
